df329f9ee6fd68a41160edfd663f8e2139c2b45c
Agarwal <vanshika.agarwal2703@gmail.com>
2024-12-16 16:00:34 +0530

    eureka server and pai gateway implemented

--- crop-service/src/main/java/com/agriculture/crop_service/CropServiceApplication.java
+++ crop-service/src/main/java/com/agriculture/crop_service/CropServiceApplication.java
@@ -2,12 +2,13 @@ package com.agriculture.crop_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CropServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CropServiceApplication.class, args);
 	}
-
 }
